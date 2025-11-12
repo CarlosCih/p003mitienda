c587ebd693e9fa148904de1d382f2e57923494e0
Carlos <cihcarlos258@gmail.com>
2025-11-12 11:03:13 -0600

    creacion de aplicacion y modelos

---
+++ mitienda/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+#modelo para almacenar las categorias de los productos
+class Categoria(models.Model):
+    name = models.CharField(max_length=200)
+    slug=models.SlugField(max_length=200)
+
+    class Meta:
+        ordering=['name']
+        indexes = [
+            models.Index(fields=['name']), #sirve en gestores de bases de datos para mejorar la velocidad de las consultas
+        ]
+
+    def __str__(self):
+        return self.name
+
+#modelo para almacenar los productos
+class Producto(models.Model):
+    category = models.ForeignKey(Categoria, related_name='products', on_delete=models.CASCADE)
+    name= models.CharField(max_length=200)
+    slug=models.SlugField(max_length=200)
+    image=models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description=models.TextField(blank=True)
+    price=models.DecimalField(max_digits=5, decimal_places=2)
+    available=models.BooleanField(default=True)
+    created=models.DateTimeField(auto_now=True)
+    updated=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering=['name']
+
+    def __str__(self):
+        return self.name
