1ce4dc11bb7089013444be029099768d7f2ddcdf
Carlos <cihcarlos258@gmail.com>
2025-11-12 11:21:21 -0600

    configuracion del panel administrador

--- mitienda/admin.py
+++ mitienda/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Categoria, Producto
 
 # Register your models here.
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)} #autocompleta el campo slug basado en el campo name
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display=['name','slug','price','available','created','updated']
+    list_filter=['available','created','updated']
+    list_editable=['price','available']
+    prepopulated_fields={'slug':('name',)}
